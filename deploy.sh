ceaa565135d250a8b20ddfc25f630f3d86d82f02
Thomas Soos <archrodney@gmail.com>
2020-11-22 20:45:56 +0100

    2020.11.22 20:45:56 - Finish deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Usage from clean master branch to deploy build folder to gh-pages branch on remote:
+# $ ./deploy.sh "Commit message"
+
 if [[ $# -eq 1 ]]
-then cp -a build/ ../ && git checkout gh-pages && cp -r ../build/* ./ && rm -rf ../build/ && git add . && git commit -m "$1"
+then cp -a build/ ../ && git checkout gh-pages && cp -r ../build/* ./ && rm -rf ../build/ && git add . && git commit -m "$1" && git push && git checkout master
 else echo "Add meg a commit message-t!"
 fi
