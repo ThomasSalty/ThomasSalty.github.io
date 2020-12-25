11ea617c526e446b6055c45e9e693b45a2bb3cb3
Thomas Soos <archrodney@gmail.com>
2020-12-26 00:07:06 +0100

    2020.12.26 00:07:07 - Include reset.css in css minification.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function(grunt) {
         cssmin: {
             dist: {
                 files: {
+                    'build/assets/css/reset.css': 'assets/css/reset.css',
                     'build/assets/css/main.css': 'assets/css/main.css',
                     'build/assets/css/critical.css': 'assets/css/critical.css',
                     'build/assets/css/noscript.css': 'assets/css/noscript.css'                    
