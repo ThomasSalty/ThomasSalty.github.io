f3d117f7674617ca18fd44b7150845b5bfbcc9be
Thomas Soos <archrodney@gmail.com>
2021-01-17 19:42:45 +0100

    2021.01.17 19:42:45 - Revert "2021.01.17 02:06:41 - Delete unused load event."
    This reverts commit ceb7040630be1842eee202f65db2afbd0f99b9b9.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,17 @@
 			$body.addClass('is-ie');
 
 	// Play initial animations on page load.
+		/*$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});*/
         window.addEventListener('DOMContentLoaded', function() {
 			window.setTimeout(function() {
-				$body.removeClass('is-preload');                
+				$body.removeClass('is-preload');
 			}, 100);
 		});
+    
     
 
 	// Forms.
